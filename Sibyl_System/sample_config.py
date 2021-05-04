a24208aa741b0b4c4b3830181c04a30c4a10cbb1
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-05-05 04:31:38 +0500

    Update sample_config.py

--- Sibyl_System/sample_config.py
+++ Sibyl_System/sample_config.py
@@ -2,7 +2,7 @@ from typing import Union, Optional
 
 API_ID: int = 3683760
 API_HASH: str = "753f879b9411fe8d3d3dc2eb01160eab"
-STRING_SESSION: str = "1BJWap1sBu1IDhTkbUvudZNZSirObKkNYmKQmUjAlBt3CYxHBh7hjIWSslg-aXrqL05feoZlUoV0yna4Mv3Spv7aBMYjmVDE9Wj2iapS7Vavc1ORKrqUaECeeFlwzgXEm2wIDJSW8VQCz-gxtlUMrLYOqMaSzXKVAlv4cQBw4rfRp_Kj4RnT3f6eRF-vcWZ8-2cmJj6p9UdEA1bS3kpsIMzgrykyVaobl9fNyrpB7KJ80aNVtCQ5e6ColZ7MFkMhRsyoQAw44hReKF4g8hBgdCKE81ENliw2xBIp79BEEFcGbIzHu3k9meTaoV-hVqM-nu5HoFF5xsu_lKW8czmsriVg8qTYRTxo=")
+STRING_SESSION: str = "1BJWap1sBu1IDhTkbUvudZNZSirObKkNYmKQmUjAlBt3CYxHBh7hjIWSslg-aXrqL05feoZlUoV0yna4Mv3Spv7aBMYjmVDE9Wj2iapS7Vavc1ORKrqUaECeeFlwzgXEm2wIDJSW8VQCz-gxtlUMrLYOqMaSzXKVAlv4cQBw4rfRp_Kj4RnT3f6eRF-vcWZ8-2cmJj6p9UdEA1bS3kpsIMzgrykyVaobl9fNyrpB7KJ80aNVtCQ5e6ColZ7MFkMhRsyoQAw44hReKF4g8hBgdCKE81ENliw2xBIp79BEEFcGbIzHu3k9meTaoV-hVqM-nu5HoFF5xsu_lKW8czmsriVg8qTYRTxo="
 Sibyl_logs: Union[str, int] = -1001317060675
 Sibyl_approved_logs: Union[str, int] = -1001439418980
 GBAN_MSG_LOGS: Optional[Union[str, int]] = -1001342831787
